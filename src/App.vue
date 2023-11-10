2871be9864dde9db670ef598af0c5dd7c6b53797
hessdale <hessdale96@gmail.com>
2023-11-09 18:39:59 -0800

    added simple pages and working on navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div id="logo">
+        <router-link to="/">
+          <h3>Sylvie Ellis</h3>
+        </router-link>
+      </div>
+      <div class="navigation">
+        <input type="radio" name="media_menu" class="menu_radio_button">
+        <h3>Media</h3>
+
+        <router-link to="/printmaking">printmaking</router-link>
+        <router-link to="/painting">painting</router-link>
+        <router-link to="/intermedia">intermedia</router-link>
+        <router-link to="/artistbooks">artistbooks</router-link>
+      </div>
+      <div class="navigation">
+        <input type="radio" name="media_menu" class="menu_radio_button">
+        <h3>About</h3>
+        <router-link to="/bio">bio</router-link>
+        <router-link to="/contact">contact</router-link>
+        <router-link to="/statement">statement</router-link>
+        <router-link to="/resume">resume</router-link>
+      </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -17,16 +37,23 @@
   color: #2c3e50;
 }
 
+.menu_radio_button {
+  display: none;
+}
+
+.navigation {
+  display: grid;
+}
+
 nav {
+  display: grid;
   padding: 30px;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-content: end;
 
   a {
     font-weight: bold;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="name">Sylvie Ellis</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+<style scoped>
+.name {
+  text-align: center;
+  font-size: 15vw;
+}
+</style>
